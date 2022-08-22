8fd15a3cc89f3500b926bfe907dcc5df3f68f0fb
yuliia <shaposhnyk.yu@gmail.com>
2022-08-22 14:38:45 +0200

    Push first files

---
+++ app/src/main/java/com/example/timerapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.timerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickStartTimer(View view) {
+    }
+
+    public void onClickPauseTimer(View view) {
+    }
+
+    public void onClickResetTimer(View view) {
+    }
+}
