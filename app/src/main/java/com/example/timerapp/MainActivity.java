746f0ae78d055e6cea1873a895a5d778e420cfa9
yuliia <shaposhnyk.yu@gmail.com>
2022-08-22 15:07:17 +0200

    Activity buttons & add function runTimer

--- app/src/main/java/com/example/timerapp/MainActivity.java
+++ app/src/main/java/com/example/timerapp/MainActivity.java
@@ -3,22 +3,59 @@ package com.example.timerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int seconds = 0;
+    private boolean isRunning = false;
+
+    private TextView textViewTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViewTimer = findViewById(R.id.tvTimer);
+        runTimer();
     }
 
     public void onClickStartTimer(View view) {
+        isRunning = true;
+
     }
 
     public void onClickPauseTimer(View view) {
+        isRunning = false;
     }
 
     public void onClickResetTimer(View view) {
+        isRunning = false;
+        seconds = 0;
+    }
+
+    private void runTimer(){
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int house = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+
+                String time = String.format(Locale.getDefault(),"%02d:%02d:%02d", house, minutes,secs);
+                textViewTimer.setText(time);
+
+                if (isRunning){
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+
     }
 }
